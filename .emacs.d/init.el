f6850a9a3213c837d1c9efdde3e787edf8393de9
Trent Robbins <robbintt@gmail.com>
2017-09-29 13:10:40 -0700

    Fixed the autogenerated package-selected-packages to be synchronized with use-packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -56,7 +56,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (solarized-theme ## evil))))
+ '(package-selected-packages (quote (helm evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
